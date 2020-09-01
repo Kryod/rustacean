ca4405d29990b97b6c2c84751cfe9f0d53a3c666
CodingLappen <reincke.tom@googlemail.com>
2020-09-01 22:21:21 +0200

    Next one

--- src/commands/exec/mod.rs
+++ src/commands/exec/mod.rs
@@ -411,9 +411,9 @@ fn exec(ctx: &mut Context, msg: &Message) -> CommandResult {
             	let tmp = tmp_split.take(2).collect::<Vec<_>>()[0] ;
             	code.insert_str(0,tmp);}
 	    else{
-		    let tmp = tmp_split.clone().take(2).collect::<Vec<_>>()[0] ;
+		    let tmp = tmp_split.clone().take(2).collect::<Vec<_>>()[1] ;
             	code.insert_str(0,tmp);
-		    let tmp_1 = tmp_split.take(2).collect::<Vec<_>>()[1] ;
+		    let tmp_1 = tmp_split.take(2).collect::<Vec<_>>()[2] ;
             	code.insert_str(0,tmp_1);
             }
         }
