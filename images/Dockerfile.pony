b4ff040a7a27ee70897032fa17221d87e8fee02f
CodingLappen <--replace>
2020-09-09 19:35:58 +0200

    Final Pony commit.

--- images/Dockerfile.pony
+++ images/Dockerfile.pony
@@ -1,1 +1,23 @@
-FROM ponylang/ponyc:latest
+FROM ubuntu:20.04
+
+ENV PATH "/root/.local/share/ponyup/bin:$PATH"
+
+RUN apt-get update \
+ && apt-get install -y --no-install-recommends \
+    ca-certificates \
+    clang \
+    curl \
+    g++ \
+    git \
+    make \
+  && rm -rf /var/lib/apt/lists/*
+
+RUN sh -c "$(curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/ponylang/ponyup/latest-release/ponyup-init.sh)" \
+ && ponyup update ponyc nightly --platform=ubuntu20.04 \
+ && ponyup update stable nightly \
+ && ponyup update corral nightly \
+ && ponyup update changelog-tool nightly
+
+RUN sh -c ""
+
+WORKDIR /src/main
--- src/commands/exec/pony.rs
+++ src/commands/exec/pony.rs
@@ -7,7 +7,7 @@ pub struct Pony;
 
 impl Language for Pony {
     fn get_image_name(&self) -> String {
-        "ponylang/ponyc".into()
+        "rustacean-pony".into()
     }
 
     fn get_lang_name(&self) -> String {
@@ -21,16 +21,20 @@ impl Language for Pony {
     fn pre_process_code(&self, code: &str, _src_path: &PathBuf) -> Option<String> {
         use regex::Regex;
 
-        let re = Regex::new(r"(?s)(actor\s+main\s*\n+^new create\(.*\)\s=>)/m").unwrap();
+        let re = Regex::new(r"(?s)(actor\s+Main\s*\n+^new create\(.*\)\s=>)/m").unwrap();
         if !re.is_match(&code) {
-            let result = format!("use \"stdlib\"\r\nuse \"random\"\r\nuse \"serialize\"\r\n use \"options\"\r\n use \"file\"\r\nuse \" builtin\"\r\nuse \"base64\"\r\nuse \"json\"\r\nuse \"itertools\"\r\nuse \"logger\"\r\n use\"math\"\r\nuse \"format\"\r\nactor main\r\nnew create(env: Env)=>\r\n{}", code);
+            // let result = format!("use \"stdlib\"\r\nuse \"random\"\r\nuse \"serialize\"\r\n use \"options\"\r\n use \"file\"\r\nuse \" builtin\"\r\nuse \"base64\"\r\nuse \"json\"\r\nuse \"itertools\"\r\nuse \"logger\"\r\n use\"math\"\r\nuse \"format\"\r\nactor Main\r\nnew create(env: Env)=>\r\n{}", code);
+		let result = format!(" use \"random\"\r\n use \"json\" \r\n use \"itertools\"\r\n actor Main\r\nnew create(env: Env)=>\r\n{}",code);
             return Some(result);
         }
 
         None
     }
+    fn get_execution_command(&self, path:&PathBuf) -> String {
+		String::from(path.to_str().unwrap())
+	}
 
-    fn get_compiler_command(&self, src_path: &PathBuf, _ : &PathBuf) -> Option<String> {
+    fn get_compiler_command(&self, src_path: &PathBuf, dest_path : &PathBuf) -> Option<String> {
         let c;
         match src_path.is_dir() {
             false => {
@@ -40,25 +44,18 @@ impl Language for Pony {
                 c=src_path; 
             }
         }
+	println!("{}", c.to_str().unwrap());
         Some(format!(
             "ponyc {} ",
                 c.to_str().unwrap()
             ))
-    }
-    fn get_execution_command(&self, path: &PathBuf) -> String {
+   }
+    fn get_out_path(&self, src_path: &PathBuf) -> PathBuf {
+	let mut t = src_path.to_path_buf();
+	t.pop();
+	//println!("{0}/pony",t.to_str().unwrap());
 
-        let my_path : &PathBuf;
-        let tmp;
-        match path.is_dir() {
-            false =>{
-                my_path= path;
-            },
-            true => {
-                tmp= path.with_file_name("pony");
-                my_path= &tmp;
-        }
-        }
-        return String::from(format!("{}", my_path.to_str().unwrap()));
+	PathBuf::from(format!("{0}/{0}",t.to_str().unwrap()))
     }
 
     fn check_compiler_or_interpreter(&self) -> String {
